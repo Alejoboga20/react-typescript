f3eddb1212b166f1b254cbdb9c6a822b10c59e15
Alejo Boga <alejoboga20@gmail.com>
2021-06-16 12:59:27 -0300

    Working with basics types

--- src/typescript/BasicsTypes.tsx
+++ src/typescript/BasicsTypes.tsx
@@ -1,9 +1,17 @@
-import React from 'react'
-
 export const BasicsTypes = () => {
+
+  const name: string = 'Alejo';
+  const age: number = 25;
+  const isActive: boolean = true;
+
+  const powers: string[] = ['Velocity', 'Strenght', 'Fly']
+
   return (
     <>
       <h3>Basics Types</h3>
+      {name}, {age}: {isActive && 'active'}
+      <br />
+      {powers.join(', ')}
     </>
   )
 }
