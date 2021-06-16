2b4503eba3bd30bcef942f512d1478b96399dc76
Alejo Boga <alejoboga20@gmail.com>
2021-06-16 12:49:26 -0300

    Adding first file

--- src/App.tsx
+++ src/App.tsx
@@ -1,7 +1,13 @@
+import { BasicsTypes } from './typescript/BasicsTypes';
+
 const App = () => {
   return (
     <div className="mt-2">
       <h1>Intro to Typescript - React</h1>
+      
+      <hr/>
+      
+      <BasicsTypes/>
     </div>
   )
 }
---
+++ src/typescript/BasicsTypes.tsx
@@ -0,0 +1,9 @@
+import React from 'react'
+
+export const BasicsTypes = () => {
+  return (
+    <>
+      <h3>Basics Types</h3>
+    </>
+  )
+}
