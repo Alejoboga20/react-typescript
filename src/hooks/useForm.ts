506fdeec1bc385eeb8449687a58a78936ee5e308
Alejo Boga <alejoboga20@gmail.com>
2021-06-21 10:43:47 -0300

    using keyof for field validation

--- src/hooks/useForm.ts
+++ src/hooks/useForm.ts
@@ -3,7 +3,7 @@ import { useState } from 'react';
 export const useForm = <T extends Object>(form: T) => {
   const [state, setState] = useState(form);
 
-  const onChange = (value: string, field: string) => {
+  const onChange = (value: string, field: keyof T) => {
     setState({
       ...state,
       [field]: value
