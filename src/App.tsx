4cf53603651fe2bb099365c11772714f3ad1fb16
Alejoboga20 <alejoboga20@gmail.com>
2021-11-07 14:52:48 -0300

    Cleaning project

--- src/App.tsx
+++ src/App.tsx
@@ -1,29 +1,10 @@
-import { Counter } from './components/Counter';
-import { BasicsTypes } from './typescript/BasicsTypes';
-import { Functions } from './typescript/Functions';
-import { LiteralObjects } from './typescript/LiteralObjects';
-import { CounterWithHook } from './components/CounterWithHook';
-import { Login } from './components/Login';
-import { Users } from './components/Users';
-import { Forms } from './components/Forms';
-
 const App = () => {
-  return (
-    <div className='mt-2'>
-      <h1>Intro to Typescript - React</h1>
-
-      <hr />
-
-      <BasicsTypes />
-      <LiteralObjects />
-      <Functions />
-      <Counter />
-      <CounterWithHook />
-      <Login />
-      <Users />
-      <Forms />
-    </div>
-  );
+	return (
+		<div className='mt-2'>
+			<h1>Intro to Typescript - React</h1>
+			<hr />
+		</div>
+	);
 };
 
 export default App;
--- src/components/Forms.tsx
+++ src/components/Forms.tsx
@@ -2,37 +2,33 @@ import { useState } from 'react';
 import { useForm } from '../hooks/useForm';
 
 export const Forms = () => {
-  const {
-    state: form,
-    email,
-    password,
-    onChange
-  } = useForm({
-    email: 'test@test.com',
-    password: '123456'
-  });
+	const {
+		state: form,
+		email,
+		password,
+		onChange,
+	} = useForm({
+		email: 'test@test.com',
+		password: '123456',
+	});
 
-  return (
-    <>
-      <h3>Forms</h3>
-      <input
-        type='text'
-        className='form-control'
-        placeholder='Email'
-        value={email}
-        onChange={({ target }) => onChange(target.value, 'email')}
-      />
-      <input
-        type='text'
-        className='form-control mt-2 mb-2'
-        placeholder='Password'
-        value={password}
-        onChange={({ target }) => onChange(target.value, 'password')}
-      />
-
-      <code>
-        <pre>{JSON.stringify(form, '', 2)}</pre>
-      </code>
-    </>
-  );
+	return (
+		<>
+			<h3>Forms</h3>
+			<input
+				type='text'
+				className='form-control'
+				placeholder='Email'
+				value={email}
+				onChange={({ target }) => onChange(target.value, 'email')}
+			/>
+			<input
+				type='text'
+				className='form-control mt-2 mb-2'
+				placeholder='Password'
+				value={password}
+				onChange={({ target }) => onChange(target.value, 'password')}
+			/>
+		</>
+	);
 };
